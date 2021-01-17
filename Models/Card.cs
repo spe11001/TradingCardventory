855bceedf7f4b2f9f881fff54e330a48a1860d8d
Spe11001 <sean.eustace@uconn.edu>
2021-01-17 16:17:48 -0500

    Created Card and User models

---
+++ Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradingCardventory.Models
+{
+    public class Card
+    {
+        public string CardId { get; set; }
+        public string CardName { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradingCardventory.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<Card> UserCards { get; set; }
+    }
+}
